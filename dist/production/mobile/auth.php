7a40f66167f3f8e2d5e39737f2bb7a75b6194ee0
byzdata <31576827+byzdata@users.noreply.github.com>
2026-06-11 01:02:07 +0000

    deploy Thu Jun 11 01:02:07 UTC 2026

--- dist/production/mobile/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!isset($include_prefix)) {
-    $include_prefix = __DIR__ . '/../';
-}
-
-$auth_redirect = '../index.php?view=mobile/index';
-include_once $include_prefix . 'lib/auth.guard.php';
